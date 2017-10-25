e79ee5803d0d655823ddaba3e2cb366f34f2443f
magicinthesky <31857879+magicinthesky@users.noreply.github.com>
2017-10-25 18:03:06 -0500

    Update movieratingtype.php

--- mr/movieratingtype.php
+++ mr/movieratingtype.php
@@ -5,19 +5,19 @@ $a= intval($_GET['a']);
 $mid= intval($_GET['mid']);
 
 if(!empty($a)){
-  $con = mysqli_connect("localhost","bluemagic_admin","heng#hao@haha!" , "bluemagic_movierating");
+  $con = mysqli_connect("localhost","#example","#example" , "#example");
   $sql="INSERT INTO `Arating` (`movie_ID`,`A_rating`) VALUES ('$mid','$a')";
   $result = mysqli_query($con,$sql);
   echo "<span> &nbsp&nbsp&nbsp&nbsp&nbsp Rated it!</span>";
 }
 if(!empty($sp)){
-  $con = mysqli_connect("localhost","bluemagic_admin","heng#hao@haha!" , "bluemagic_movierating");
+  $con = mysqli_connect("localhost","#example","#example" , "#example");
   $sql="INSERT INTO `Sprating` (`movie_ID`,`sp_rating`) VALUES ('$mid','$sp')";
   $result = mysqli_query($con,$sql);
   echo "<span> &nbsp&nbsp&nbsp&nbsp&nbsp Rated it!</span>";
 }
 if(!empty($v)){
-  $con = mysqli_connect("localhost","bluemagic_admin","heng#hao@haha!" , "bluemagic_movierating");
+  $con = mysqli_connect("localhost","#example","#example" , "#example");
   $sql="INSERT INTO `Vrating` (`movie_ID`,`v_rating`) VALUES ('$mid','$v')";
   $result = mysqli_query($con,$sql);
   echo "<span> &nbsp&nbsp&nbsp&nbsp&nbsp Rated it!</span>";
